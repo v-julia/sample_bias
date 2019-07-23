ba2074851ad68c3aa1c4fe040f827404dd01fc32
v-julia <vjulia94@gmail.com>
2019-07-23 15:55:11 +0300

    loaded random_sample.py

---
+++ random_sample.py
@@ -0,0 +1,123 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+import seaborn as sns
+#from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio import AlignIO
+from Bio import SeqIO
+
+'''
+Эта программа создает выборку следующим образом:
+Все последовательности разбиваются на группы по первым 4 знакам genbank ID. 
+Затем случайно выбираем группу. Из этой группы все последовательности добавляем в новую выборку, пока последовательностей не станет, сколько нужно.
+Если последовательностей меньше, чем 
+'''
+# input_file - file with sequences in fasta-format used to generate random samples
+# output_dir - output directory for generated files
+# N_align - number of alignments to generate
+# N_seq_max - number of sequences in generated alignment
+# threshold - the number of letters that should coincide in 
+# GB Accessions of two sequences to join them into one group
+
+def gen_random_sample3(input_file, output_dir, N_align, N_seq_max, threshold):
+
+    input_file = '/'.join(input_file.split('\\')) # change path to linux style
+    aln = list(SeqIO.parse(open(input_file), 'fasta'))
+
+    #Dictionary with groups which gb ids start with the same string
+    ids_dict = {}
+
+    for rec in aln:
+        #print(rec)
+        group_id = rec.id[0:threshold] #id of new group
+        if group_id in ids_dict.keys():
+            ids_dict[group_id].append(rec.id)
+        else:
+            ids_dict[group_id] = [rec.id]
+    
+    '''
+    #this code is for printing length of group and Genbank accessions of sequences that belong to each group
+    l_gsize = [] #list with group sizes
+    for key in sorted(ids_dict.keys()):
+        #print(key + ','+ str(len(ids_dict[key])))
+        l_gsize.append(len(ids_dict[key]))
+        #for entr in ids_dict[key]:
+        #    print('\t'+entr)
+    #print(len(ids_dict.keys()))
+
+    #print(sorted(l_gsize))
+    sns.set()
+    plt.hist(l_gsize)
+    plt.title("Distribution of group sizes")
+    plt.xlabel("Size of group")
+    plt.ylabel("Number of sequences")
+    plt.show()
+    '''
+
+    for j in range(N_align):
+        
+        print('Generating {} file'.format(str(j+1)))
+        
+        #current size of random sample
+        Sample_size = 0 
+        #list with ids in random sample
+        sample_ids = [] 
+        #list with random groups that has been chosen in loop
+        random_groups = []
+        #alignment objects of sequences that will be added to random sample
+        list_aln_obj = []
+        
+        while Sample_size<N_seq_max:
+            print("Current size of alignment {}".format(Sample_size))
+            #choose random group
+            random_group = random.choice(list(ids_dict.keys()))
+
+            #If the group has already been chosen we continue our search
+            if random_group in random_groups:
+                continue
+            else:
+                print(random_group, len(ids_dict[random_group]))
+                random_groups.append(random_group)
+                #add all sequences from the group to random sample
+                
+                for id in ids_dict[random_group]:
+                    if len(sample_ids) < N_seq_max:
+                        sample_ids.append(id)
+                    else:
+                        break
+            Sample_size = len(sample_ids)
+
+
+        for rec in aln:
+
+            if rec.id  in sample_ids:
+                list_aln_obj.append(rec)
+                
+
+        output_file_n = ".".join(input_file.split("/")[-1].split(".")[:-1])+"_3random_"+str(j)+".fasta"
+        if output_dir == None:
+            output_dir = "/".join(input_file.split("/")[:-1]) + "/"
+        output = output_dir + output_file_n
+        AlignIO.write(AlignIO.MultipleSeqAlignment(list_aln_obj), output, "fasta")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-input", "--input_file", type=str,
+                        help="Input file", required=True)
+    parser.add_argument("-out_dir", "--output_dir", type=str,
+                        help="Output directory to save generated alignments")
+    parser.add_argument("-n_samp", "--n_samples", type=int,
+                        help="Number of random alignments to generate", required=True)
+    parser.add_argument("-n_seq_max", "--n_seq_max", type=int,
+                        help="Number of sequences in generated alignment", required=True)
+    parser.add_argument("-threshold", "--threshold", type=int,
+                        help="Input file", required=True)
+    args = parser.parse_args()
+
+
+    gen_random_sample3(args.input_file, args.output_dir, args.n_samples, args.n_seq_max, args.threshold)
+
+
